388f5e26a9f60749e6d5263585f33fc164ad1d11
Pizza_Man <62097126+Pizza-Man217@users.noreply.github.com>
2020-09-29 19:37:19 +0800

    Add files via upload

---
+++ d039.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(0);cin.tie(0);
+	
+	int T;
+	cin>>T;
+	while(T--){
+		int n,m;
+		cin>>n>>m;
+		cout<<(n/3)*(m/3)<<'\n';
+	}
+}
